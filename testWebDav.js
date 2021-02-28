46555c2e59e98d177d2711cad764948ad1a73db8
Jan Ryklikas <jry@facelift-bbt.com>
2021-02-28 17:35:44 +0100

    improved local testing including test data

--- testWebDav.js
+++ testWebDav.js
@@ -2,34 +2,19 @@
 const { AuthType, createClient } = require("webdav");
 const ical = require('node-ical');
 const transformer = require("./transformer");
+const { fetchList, parseList } = require("./webDavHelper");
 const config = require("./testConfig");
+const icsList = require("./testDataTemplate");
 
-var todos = [];
+async function test() {
+    var todos = [];
 
-const client = createClient(config.listUrl, config.webDavAuth);
+    //const icsList = await fetchList(config);
+    todos = parseList(icsList);
+    todos = transformer.transformData(todos);
 
-async function getData() {
-    try {
-        const directoryItems = await client.getDirectoryContents("/");
-
-        for (const element of directoryItems) {
-            await readData(element.filename)
-        }
-        todos = transformer.transformData(todos);
-        console.log(todos);
-    } catch (error) {
-        if(error.status === 401) {
-            console.error("Unauthorized");
-        }
-    }
+    console.log(todos);
+    console.log(JSON.stringify(todos));
 }
 
-async function readData(filename) {
-    const str = await client.getFileContents(filename, { format: "text" });
-    const directEvents = ical.sync.parseICS(str);
-    Object.values(directEvents).forEach(element => {
-        if (element.type === 'VTODO') todos.push(element);
-    });
-}
-
-getData();
+test();
